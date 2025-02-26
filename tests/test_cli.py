f440a3b2eb0752b05f802f989631b3a7865c4920
Marcell Douglas Nagy <133755508+marcelldls@users.noreply.github.com>
2025-02-26 15:03:39 +0000

    Add testing ci (#23)
    * Add system tests
    
    * Add minikube action
    
    * Fix DOCKER_PROVIDER get env
    
    * Pull ubuntu before running CI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,16 @@
 import subprocess
-import sys
 
+from data import responses
 from kodman import __version__
+
+ENTRY_POINT = "kodman"
 
 
 def test_cli_version():
-    cmd = [sys.executable, "-m", "kodman", "--version"]
+    cmd = [ENTRY_POINT, "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
+
+
+def test_cli_help():
+    cmd = [ENTRY_POINT, "--help"]
+    assert subprocess.check_output(cmd).decode().strip() == responses.help_screen
---
+++ tests/test_system.py
@@ -0,0 +1,122 @@
+import os
+import subprocess
+from pathlib import Path
+
+import pytest
+
+from data import responses
+
+ENTRY_POINT = "kodman"
+DOCKER_PROVIDER = os.getenv("DOCKER_PROVIDER", "podman")
+KODMAN_SYSTEM_TESTING = os.getenv("KODMAN_SYSTEM_TESTING") == "true"
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def remove_empty_lines(text):
+    """Remove after solving https://github.com/epics-containers/Kodman/issues/9"""
+    lines = [line for line in text.split("\n") if line.strip()]
+    return "\n".join(lines)
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_docker_run_hello():
+    cmd = [DOCKER_PROVIDER, "run", "--rm", "hello-world"]
+    assert subprocess.check_output(cmd).decode().strip() == responses.hello_world
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_kodman_run_hello():
+    cmd = [ENTRY_POINT, "run", "--rm", "hello-world"]
+    assert subprocess.check_output(cmd).decode().strip() == remove_empty_lines(
+        responses.hello_world
+    )
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_docker_run_exitcodes():
+    ERROR_MSG = "My error message"
+    cmd = [
+        DOCKER_PROVIDER,
+        "run",
+        "--entrypoint",
+        "bash",
+        "--rm",
+        "ubuntu",
+        "-c",
+        f"echo '{ERROR_MSG}' >&2; exit 1",
+    ]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert result.returncode == 1
+    assert result.stderr.strip() == ERROR_MSG
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_kodman_run_exitcodes():
+    ERROR_MSG = "My error message"
+    cmd = [
+        ENTRY_POINT,
+        "run",
+        "--entrypoint",
+        "bash",
+        "--rm",
+        "ubuntu",
+        "-c",
+        f"echo '{ERROR_MSG}' >&2; exit 1",
+    ]
+
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    assert result.returncode == 1
+    assert (
+        result.stdout.strip() == ERROR_MSG
+    )  # K8s does not distinguish between stderr and stdout!
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_docker_run_mount(data: Path):
+    FILE_MOUNT = "to_mount.txt"
+    cmd = [
+        DOCKER_PROVIDER,
+        "run",
+        "-v",
+        f"{data}:/test",
+        "--rm",
+        "ubuntu",
+        "bash",
+        "-c",
+        f"cat test/{FILE_MOUNT}",
+    ]
+
+    assert subprocess.check_output(cmd).decode().strip() == responses.mount
+
+
+@pytest.mark.skipif(
+    not KODMAN_SYSTEM_TESTING, reason="export KODMAN_SYSTEM_TESTING=true"
+)
+def test_kodman_run_mount(data: Path):
+    FILE_MOUNT = "to_mount.txt"
+    cmd = [
+        ENTRY_POINT,
+        "run",
+        "-v",
+        f"{data}:/test",
+        "--rm",
+        "ubuntu",
+        "bash",
+        "-c",
+        f"cat test/{FILE_MOUNT}",
+    ]
+
+    assert subprocess.check_output(cmd).decode().strip() == responses.mount
